ddd8f2f783cacfaa7a6c3e0a93980909a35888cf
rheajoywalters <54914771+rheajoywalters@users.noreply.github.com>
2019-11-25 11:50:49 -0700

    TravelBoard Initial Commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyDnT749pto7K9C5N6VvS0aTVvFMJq6QSzI",
-  authDomain: "authentication-d0e33.firebaseapp.com",
-  databaseURL: "https://authentication-d0e33.firebaseio.com",
-  projectId: "authentication-d0e33",
-  storageBucket: "",
-  messagingSenderId: "310675757578",
-  appId: "1:310675757578:web:9e17f766afbce2075bddf3"
+  apiKey: "AIzaSyBaiteZltjb5655T_z9L4GQo_QoWJstVqo",
+  authDomain: "travel-board-9209b.firebaseapp.com",
+  databaseURL: "https://travel-board-9209b.firebaseio.com",
+  projectId: "travel-board-9209b",
+  storageBucket: "travel-board-9209b.appspot.com",
+  messagingSenderId: "398163510800",
+  appId: "1:398163510800:web:5f422fad2b841a35ff6c95",
+  measurementId: "G-5P08FXB23N"
 };
 
 firebase.initializeApp(config);
